0fa1a8f44cc8b05eaeca2bacbd3f0ea01839d5a9
Rob Murphy <rmurphyswimmer@gmail.com>
2022-03-08 15:59:10 +0100

    built all the pieces for getting image urls from twitter. Next step is to add this to the blazor app.

--- CatFinder.Server/Services/Sources.cs
+++ CatFinder.Server/Services/Sources.cs
@@ -1,10 +1,30 @@
 ﻿
+using TwitterSharp.Client;
+using TwitterSharp.Request;
+using TwitterSharp.Request.AdvancedSearch;
+using TwitterSharp.Response.RMedia;
+using TwitterSharp.Response.RTweet;
+using TwitterSharp.Rule;
+
 namespace CatFinder.Server.Services;
 
-public class Sources 
+public class Sources
 {
-    public object[] GetLatestPictures(int numberOfPictures)
+    private readonly TwitterClient _client;
+    public Sources()
     {
-        return new object[numberOfPictures];
+        _client = new TwitterSharp.Client.TwitterClient("AAAAAAAAAAAAAAAAAAAAAOIFaAEAAAAASR8x255u%2FCUOYC5zP7MsO7Am%2BcU%3DHAMGkBeeHuBnprrkAzlyKanCCHW4Jr6KDlGZLniDGcgsGyNATY");
+
+    }
+    public async void GetLatestPictures()
+    {
+        var streamInfo = _client.AddTweetStreamAsync(new StreamRequest(Expression.HasImages(),"cat"));
+        var onTweetReceived = ScrapeMediaFromTweet;
+        await _client.NextTweetStreamAsync(onTweetReceived, mediaOptions: new MediaOption[] {MediaOption.Url});
+    }
+
+    private void ScrapeMediaFromTweet(Tweet tweet)
+    {
+        var images = tweet.Attachments.Media.Where(x => x.Type == MediaType.Photo).Select(x => x.Url).ToArray();
     }
 }
