3655f57ce8bb0b15f9cf1069a09580ac93b42cc8
Rob Murphy <rmurphyswimmer@gmail.com>
2022-03-08 14:30:06 +0100

    initial basic test

---
+++ CatFinder.Server/Services/Sources.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CatFinder.Server.Services;
+
+public class Sources 
+{
+    public object[] GetLatestPictures(int numberOfPictures)
+    {
+        return new object[numberOfPictures];
+    }
+}
---
+++ tests/CatFinder.Tests/Features/Sources.feature
@@ -0,0 +1,5 @@
+﻿Feature: Picture retrieval from sources
+
+Scenario: Pictures are retrieved
+	When the Sources are asked for their latest 100 pictures
+	Then the latest 100 pictures are returned
