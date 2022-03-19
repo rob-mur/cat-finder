97a58bc76a8763c1ea83ecfb57e778e96426c576
Rob Murphy <rmurphyswimmer@gmail.com>
2022-03-19 17:07:01 +0100

    using prediction api

--- CuteAnimalFinder/Services/PredictionService.cs
+++ CuteAnimalFinder/Services/PredictionService.cs
@@ -1,48 +1,23 @@
-﻿using System.Net;
-using CuteAnimalFinder.Models;
-using Keras;
-using Keras.Models;
+﻿using CuteAnimalFinder.Models;
+using Newtonsoft.Json;
 
 namespace CuteAnimalFinder.Services;
 
 public class Prediction : IPrediction
 {
-    private readonly BaseModel _model;
-    public Prediction()
+    private readonly string _predictionUrl;
+    public Prediction(IConfiguration config)
     {
-        Console.WriteLine("Initialising model");
-        _model = BaseModel.LoadModel("cat_dog_neither.h5");
-        Console.WriteLine("Model initialised");
+        _predictionUrl = config.GetSection("PredictionURL").Value!;
     }
     public string[] FilterImages(Animal search, string[] images)
     {
-        // TODO: this section should call a python FastAPI because the C~ Keras bindings suck ass
-        string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-        Directory.CreateDirectory(tempDirectory);
-        var filteredImages =  images.Where(image =>
-        {
-            // Download image
-            var fileName = Path.Combine(tempDirectory, Path.GetRandomFileName());
-            Console.WriteLine($"Downloading {fileName}");
-#pragma warning disable CS0618
-            using var client = new WebClient();
-#pragma warning restore CS0618
-            client.DownloadFile(image, fileName);
-            Console.WriteLine("Finished downloading");
-            // Run model on image
-            var loadedImage = Keras.PreProcessing.Image.ImageUtil.LoadImg(fileName, target_size: new Shape(224, 224));
-            var imageArray = Keras.PreProcessing.Image.ImageUtil.ImageToArray(loadedImage);
-            // TODO: potentially add a section that centers the image based on the training mean used
-            Console.WriteLine("Predicting begin");
-            var output = _model.Predict(imageArray);
-            Console.WriteLine("Predicting end");
-            output = output[0];
-            // Check if prediction matches search
-            Console.WriteLine($"Checking that {output} is equal to {search}");
-            return output == search;
-        }).ToArray();
-        Directory.Delete(tempDirectory,true);
-        return filteredImages;
+        var query = "?urls=" + String.Join("&urls=", images);
+        using var client = new HttpClient();
+        var response = client.GetAsync(new Uri(_predictionUrl + query)).Result;
+        var responseString = response.Content.ReadAsStringAsync().Result;
+        var result = JsonConvert.DeserializeObject<int[]>(responseString)!;
+        return images.Where((x, i) => result[i] == (int) search).ToArray();
     }
 }
 
