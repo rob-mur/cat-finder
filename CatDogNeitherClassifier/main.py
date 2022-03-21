f6b4636c2043fdf70b77ff019774c8a7ecddf3c3
Rob Murphy <rmurphyswimmer@gmail.com>
2022-03-21 20:45:55 +0100

    updated

--- CatDogNeitherClassifier/main.py
+++ CatDogNeitherClassifier/main.py
@@ -3,7 +3,7 @@ import sys
 
 import tensorflow as tf
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, Input
 from keras.models import Model
 from keras.optimizer_v1 import rmsprop
 from keras.preprocessing.image import img_to_array
@@ -16,8 +16,7 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 import keras
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-
-#tf.compat.v1.disable_eager_execution()
+tf.compat.v1.disable_eager_execution()
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
@@ -56,11 +55,16 @@ def define_model():
     model = InceptionV3(input_shape=(150, 150, 3), include_top=False, weights='imagenet')
     for layer in model.layers:
         layer.trainable = False
-    x = Flatten()(model.output)
+    i = Input([None, None, 3], dtype=tf.uint8)
+    x = tf.cast(i, tf.float32)
+    x = tf.keras.applications.inception_v3.preprocess_input(x)
+    x = model(x)
+
+    x = Flatten()(x)
     x = Dense(1024, activation='relu')(x)
     x = Dropout(0.2)(x)
     predictions = Dense(3, activation='sigmoid')(x)
-    model = Model(inputs=model.inputs, outputs=predictions)
+    model = Model(inputs=i, outputs=predictions)
     # compile model
     opt = rmsprop(lr=0.0001)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
@@ -68,22 +72,22 @@ def define_model():
 
 
 def productionise_model():
-    es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=10)
+    es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=50)
     mc = ModelCheckpoint('best_model.h5', monitor='val_accuracy', mode='max')
     cb_list = [es, mc]
     # define model
     model = define_model()
     # create data generator
-    train_datagen = ImageDataGenerator(rescale=1. / 255., rotation_range=40, width_shift_range=0.2,
+    train_datagen = ImageDataGenerator(rotation_range=40, width_shift_range=0.2,
                                        height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-    test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+    test_datagen = ImageDataGenerator()
     # prepare iterators
     train_it = train_datagen.flow_from_directory(resTrainingRoot,
                                                  batch_size=32, target_size=(150, 150))
     test_it = test_datagen.flow_from_directory(resTestRoot, batch_size=32, target_size=(150, 150))
     # fit model
     history = model.fit(train_it, steps_per_epoch=len(train_it),
-                        validation_data=test_it, validation_steps=len(test_it), epochs=100, callbacks=cb_list,
+                        validation_data=test_it, validation_steps=len(test_it), epochs=1000, callbacks=cb_list,
                         verbose=1)
     summarize_diagnostics(history)
 
@@ -105,30 +109,6 @@ def summarize_diagnostics(history):
     pyplot.close()
 
 
-def freeze_best_model():
-    model = keras.models.load_model("best_model.h5")
-    full_model = tf.function(lambda x: model(x))
-    full_model = full_model.get_concrete_function(
-        tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
-    # Get frozen graph def
-    frozen_func = convert_variables_to_constants_v2(full_model)
-    frozen_func.graph.as_graph_def()
-    layers = [op.name for op in frozen_func.graph.get_operations()]
-    print("-" * 60)
-    print("Frozen model layers: ")
-    for layer in layers:
-        print(layer)
-    print("-" * 60)
-    print("Frozen model inputs: ")
-    print(frozen_func.inputs)
-    print("Frozen model outputs: ")
-    print(frozen_func.outputs)
-    tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
-                      logdir="frozen_models",
-                      name="frozen_graph.pb",
-                      as_text=False)
-
-
 if __name__ == "__main__":
     # define location of dataset
     dataRoot = r"C:\data\cat_dog_neither"
@@ -140,6 +120,4 @@ if __name__ == "__main__":
     finalDataRoot = os.path.join(dataRoot, "finalised_data")
     # resize_images(trainingRoot, resTrainingRoot)
     # resize_images(testRoot, resTestRoot)
-    # train_and_evaluate()
-    # productionise_model()
-    freeze_best_model()
+    productionise_model()
