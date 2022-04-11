2192a2f9ca547f20f281e3106526a9b70d4a584b
Rob Murphy <rmurphyswimmer@gmail.com>
2022-04-11 19:31:38 +0200

    starting creating a mirror table that uses sha1

--- CuteAnimalFinder/PredictionDbContext.cs
+++ CuteAnimalFinder/PredictionDbContext.cs
@@ -1,37 +1,44 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text.Json.Serialization;
 using CuteAnimalFinder.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace CuteAnimalFinder;
 
-public class PredictionDbContext :  DbContext
+public class PredictionDbContext : DbContext
 {
     private readonly string _connectionString;
 
     public DbSet<ImagePrediction> Predictions { get; set; } = null!;
+    public DbSet<NewImagePrediction> NewPredictions { get; set; } = null!;
 
     public PredictionDbContext(IConfiguration config)
     {
         _connectionString = config.GetSection("PredictionCacheCS").Value!;
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NewImagePrediction>().Property(b => b.Votes).HasConversion(
+            v => JsonConvert.SerializeObject(v), v => JsonConvert.DeserializeObject<Dictionary<Animal, int>>(v));
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(_connectionString);
-        
     }
-    
-    private Task AddPrediction(ImagePrediction prediction)
+
+    private async Task AddPrediction(ImagePrediction prediction)
     {
         Predictions.Add(prediction);
-        return Task.CompletedTask;
     }
 }
 
 public class ImagePrediction
 {
-    [Key]
-    public string Url { get; init; }
+    [Key] public string Url { get; init; }
     public Animal Prediction { get; init; }
 
     public ImagePrediction(string url, Animal prediction)
@@ -39,4 +46,17 @@ public class ImagePrediction
         Url = url;
         Prediction = prediction;
     }
+}
+
+public class NewImagePrediction
+{
+    public NewImagePrediction(byte[] sha1, Dictionary<Animal, int> votes)
+    {
+        Sha1 = sha1;
+        Votes = votes;
+    }
+
+    [Key] public byte[] Sha1 { get; init; }
+
+    public Dictionary<Animal, int> Votes { get; init; }
 }
--- CuteAnimalFinder/Services/DbService.cs
+++ CuteAnimalFinder/Services/DbService.cs
@@ -1,4 +1,7 @@
-﻿namespace CuteAnimalFinder.Services;
+﻿using System.Security.Cryptography;
+using CuteAnimalFinder.Models;
+
+namespace CuteAnimalFinder.Services;
 
 public class DbService: IDbService
 {
@@ -7,10 +10,31 @@ public class DbService: IDbService
     {
         _db = new PredictionDbContext(config);
     }
-    public Task AddPrediction(ImagePrediction imagePrediction)
+    public async Task AddPrediction(ImagePrediction imagePrediction)
     {
         _db.Predictions.Add(imagePrediction);
-        return Task.CompletedTask;
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(imagePrediction.Url);
+        var imageBytes = await response.Content.ReadAsByteArrayAsync();
+        var sha1 = SHA1.HashData(imageBytes);
+        var existingRecord = _db.NewPredictions.FirstOrDefault(x => x.Sha1 == sha1);
+        if (existingRecord != null)
+        {
+            existingRecord.Votes[imagePrediction.Prediction] += 1;
+            await _db.SaveChangesAsync();
+        }
+        else
+        {
+            var newRecord = new NewImagePrediction(sha1, new Dictionary<Animal, int>()
+            {
+                {Animal.Cat, 0},
+                {Animal.Dog, 0},
+                {Animal.Neither, 0}
+            });
+            newRecord.Votes[imagePrediction.Prediction] += 1;
+            await _db.AddAsync(newRecord);
+            await _db.SaveChangesAsync();
+        }
     }
 }
 
